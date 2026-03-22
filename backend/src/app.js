1d1aa2431e1e2e9a5a25c7d3d59dcb751aaf0d04
sdan-07 <soumyadipdan7@gmail.com>
2026-03-22 18:00:32 +0530

    added health check route

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,14 @@ app.use(cors())
 //import
 const noteRouter = require('./routes/note.route')
 
+//health check route
+app.get('/', (_,res)=>{
+    res.status(200).json({
+        status: "OK",
+        message: "Server is running"
+    })
+});
+
 app.use('/api/note', noteRouter)
 
 module.exports = app
